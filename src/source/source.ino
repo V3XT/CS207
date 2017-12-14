96882b4ec74b1ecd2ad402b08ca76f3c58c2bddb
V3XT <vafries@gmail.com>
2017-12-14 13:31:56 -0600

    Added Wave loop
    added a loop to generate and output a sawtooth wave or two waves and sum them. Needs work to allow generation of higher frequency waves.

--- src/source/source.ino
+++ src/source/source.ino
@@ -10,34 +10,49 @@
  * 
  */
 
+#define SAMPLERATE 8000
+
 const int waveQuality = 256;  //The maximum height of a generated wave
 const int pot1pin = A0;   //Input pin of the first potentiometer
 const int pot2pin = A1;   //Input pin of the second potentiometer
-const int pot2pinBool = A2; //Input for the toggle to control second potentiometer
+const int bothWaves = 8; //Input for the toggle to control second potentiometer
 int pot1 = 0;   //Stores the measurement of the first potentiometer
 int pot2 = 0;   //Stores the measurement of the second potentiometer
 
-
+//record an initial time
+  long sampleStop = micros();
+  char waveOne = 0, waveTwo = 0;
 void setup() {
   //Initialize pins for the DAC
   DDRD = B11111111;
+  pinMode(bothWaves,INPUT);
 
 }
 
 void loop() {
 
-  //record an initial time
-  long sampleStop = micros();
-
-   //play sound
-  for (long i = 0; i < 8000; i++)
-  {
-    //Quick way to write 8 bits to pins 0 to 7
-    PORTD = (map(analogRead(pot1pin),0,1023,5,100)*i) % waveQuality;
+  while(1){
+  
+  if(digitalRead(bothWaves)){
     
-    //wait for the right amount of time for 8000Hz
-    sampleStop += 125;
-    while(micros() < sampleStop);
+     waveOne += 128/(SAMPLERATE/(map(analogRead(pot1pin),0,1023,40,3150)));
+     waveTwo += 128/(SAMPLERATE/(map(analogRead(pot2pin),0,1023,40,3150)));
+     waveOne = waveOne % 128;
+     waveTwo = waveTwo % 128;
+
+     PORTD = waveOne + waveTwo;
+  }
+  else {
+    waveOne += 256/(SAMPLERATE/(map(analogRead(pot1pin),0,1023,40,3150)));
+    
+    PORTD = waveOne;
   }
 
+  //wait for the right amount of time for 8000Hz
+    sampleStop += 125;
+    while(micros() < sampleStop){}
+  }
 }
+
+
+
